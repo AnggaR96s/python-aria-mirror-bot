e305c097953ef00dc425fa3ad3e5c502c4eb90a4
Angga <angga@linuxmail.org>
2020-10-31 03:11:28 +0700

    main: GPXCloud bot
    Change-Id: I558ae24532c4bd829f37f5b810ae8cd589529a43
    Signed-off-by: Angga <angga@linuxmail.org>

--- bot/__main__.py
+++ bot/__main__.py
@@ -43,7 +43,7 @@ def start(update, context):
     LOGGER.info('UID: {} - UN: {} - MSG: {}'.format(update.message.chat.id,update.message.chat.username,update.message.text))
     if CustomFilters.authorized_user(update):
         if update.message.chat.type == "private" :
-            sendMessage(f"Hey <b>{update.message.chat.first_name}</b>. Welcome to <b>LoaderX Bot</b>", context.bot, update)
+            sendMessage(f"Hey <b>{update.message.chat.first_name}</b>. Welcome to <b>GPXCloud Bot</b>", context.bot, update)
         else :
             sendMessage("I'm alive :)", context.bot, update)
     else :
